c16898f209d5f26e327cd2d9d9845ca4e25479a6
Dc Dareal Sabas <dcdareal.sabas@deped.gov.ph>
2026-02-04 16:02:56 +0800

    hey

--- frontend/src/views/Tab4Page.vue
+++ frontend/src/views/Tab4Page.vue
@@ -1,13 +1,25 @@
-<template lang="">
-    <div>
-        <h1>Tab 4 Content</h1>
-    </div>
+<template>
+    <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <Header/>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header>
+        <ion-toolbar>
+          <ion-title size="large">Tab 2</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ExploreContainer name="Tab 2 page" />
+    </ion-content>
+  </ion-page>
 </template>
-<script>
+<script lang="ts">
+import ExploreContainer from "@/components/ExploreContainer.vue";
+import Header from '@/components/Header.vue';
 export default {
     
 }
 </script>
-<style lang="">
-    
-</style>
